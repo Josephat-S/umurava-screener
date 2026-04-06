56c13c7951b1ba7a38ae0761e6a48225b7a4c0d9
Josephat-S <sangwajozaphat@gmail.com>
2026-04-06 18:42:21 +0200

    changed the bg and fg colors to meet umurava standard and also the layout, but also added required dependencies

---
+++ apps/frontend/app/providers.tsx
@@ -0,0 +1,15 @@
+"use client";
+
+import type { ReactNode } from "react";
+import { Provider } from "react-redux";
+import { Toaster } from "react-hot-toast";
+import { store } from "@/store";
+
+export function Providers({ children }: { children: ReactNode }) {
+  return (
+    <Provider store={store}>
+      {children}
+      <Toaster position="top-right" />
+    </Provider>
+  );
+}
